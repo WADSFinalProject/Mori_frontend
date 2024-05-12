470ba8b6848311da6a8d77ccfddbc30d50932957
GuhD <88705355+GuhD01@users.noreply.github.com>
2024-05-12 20:49:55 +0700

    LOGINNNNNN

--- Mori/tailwind.config.js
+++ Mori/tailwind.config.js
@@ -5,17 +5,29 @@ export default {
   theme: {
     extend: {
       width: {
-        'custom': '150rem', 
+        'custom': '150rem',  // Custom width
       },
       height: {
-        'custom': '150rem', 
+        'custom': '150rem',  // Custom height
       },
       fontFamily: {
-        vietnam: ['"Be Vietnam Pro"', "sans-serif"]
+        vietnam: ['"Be Vietnam Pro"', "sans-serif"]  // Custom font family
       },
+      spacing: {
+        '90': '90px',  // Define custom spacing value for `90px`
+      },
+      keyframes: {
+        slideInRight: {
+          '0%': { transform: 'translateX(-100%)' }, // Starts from the left of the screen
+          '100%': { transform: 'translateX(0%)' },  // Moves to cover the screen
+        }
+      },
+      animation: {
+        slideInRight: 'slideInRight 0.5s ease-out forwards',
+      }
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin')  // Flowbite plugin
   ],
 };
