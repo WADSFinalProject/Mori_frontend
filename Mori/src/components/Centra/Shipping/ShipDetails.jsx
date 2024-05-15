c57c7325dbc0c20888c3535fc5f59717440b526b
GuhD <88705355+GuhD01@users.noreply.github.com>
2024-05-15 21:20:15 +0700

    Final

--- Mori/src/components/Centra/Shipping/ShipDetails.jsx
+++ Mori/src/components/Centra/Shipping/ShipDetails.jsx
@@ -10,7 +10,8 @@ import GreenTruck from '../../../assets/GreenTruck.png';
 import darkgreenpoint from '../../../assets/DarkGreenPoint.png';
 import RedTruck from '../../../assets/RedTruck.png';
 import RedPoint from '../../../assets/RedPointer.png';
-
+import neoTruck from '../../../assets/neotruck.png';  // Add your neotruck.png here
+import neoPoint from '../../../assets/neopoint.png';  // Add your neopoint.png here
 
 export default function ShipDetails() {
     const { width } = useWindowSize();
@@ -20,15 +21,14 @@ export default function ShipDetails() {
     const delivStatus = "JNE Standard - JNE102392130";
     const delivType = "Regular";
     const preparingTime = "18-03-2024 08:40 PM"; 
-    const [shipmentState, setShipmentState] = useState('Missing'); // Default state is 'Shipped'
+    const [shipmentState, setShipmentState] = useState('Shipped'); // Default state is 'Missing'
 
-    
     const batches = [
         { id: 10201, weight: '23 kg', driedDate: '11/11/25', flouredDate: '11/11/25' },
         { id: 10202, weight: '23 kg', driedDate: '11/11/25', flouredDate: '11/11/25' },
         { id: 10203, weight: '21 kg', driedDate: '11/11/25', flouredDate: '11/11/25' },
         { id: 10204, weight: '40 kg', driedDate: '11/11/25', flouredDate: '11/11/25' },
-            ];
+    ];
 
     const totalWeight = batches.reduce((acc, batch) => {
         const batchWeight = parseFloat(batch.weight.replace('kg', '').trim());
@@ -36,7 +36,7 @@ export default function ShipDetails() {
     }, 0).toFixed(1) + ' kg';
 
     return (
-        <div className={isMobile ? "mb-16" : "" }style={{ paddingBottom: '40px' }}>
+        <div className={isMobile ? "mb-16" : "" } style={{ paddingBottom: '40px' }}>
             {isMobile ? (
                 <>
                     <div className="p-2 shadow-md flex items-center justify-between bg-white">
@@ -49,11 +49,11 @@ export default function ShipDetails() {
                         </div>
                         <div className="w-4 h-4"></div>
                     </div>
-                    
+
                     {shipmentState === 'Shipped' && (
                         <>
                             <div className="p-3 shadow-md flex items-start" style={{ backgroundColor: '#B4E0CC' }}>
-                                <img src={packIcon} alt="Package" className="w-6 h-6 mr-2 " style={{ color: '#000000' }} />
+                                <img src={packIcon} alt="Package" className="w-6 h-6 mr-2" />
                                 <div className="flex flex-col justify-between">
                                     <h2 className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#217045' }}>Shipped</h2>
                                     <p className="text-xs lg:text-sm font-vietnam">Estimated Arrival <strong>{date}</strong></p>
@@ -73,7 +73,7 @@ export default function ShipDetails() {
                                             <button className="text-red-500 text-m lg:text-sm font-semibold self-start mr-2 font-vietnam" style={{ color: '#5D9EA4' }}>Track</button>
                                         </div>
                                         <div className="flex items-center">
-                                            <img src={greenpoint} alt="Red Point" className="w-3 h-7 mr-2" />
+                                            <img src={greenpoint} alt="Green Point" className="w-3 h-7 mr-2" />
                                             <div className="flex flex-col">
                                                 <span className="text-xs lg:text-sm font-bold font-vietnam" style={{ color: '#9AD1B3' }}>Preparing to ship</span>
                                                 <span className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>{preparingTime} </span>
@@ -87,11 +87,11 @@ export default function ShipDetails() {
                     {shipmentState === 'Delivering' && (
                         <>
                             <div className="p-3 shadow-md flex items-start" style={{ backgroundColor: '#4D946D' }}>
-                                <img src={GreenTruck} alt="Package" className="w-6 h-5 mr-2 " style={{ color: '#000000' }} />
+                                <img src={GreenTruck} alt="Package" className="w-6 h-5 mr-2" />
                                 <div className="flex flex-col justify-between">
                                     <h2 className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#B8F3D3' }}>To deliver</h2>
                                     <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#FFFFFF' }}>Estimated Arrival <strong>{date}</strong></p>
-                                    <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#F0F0F0' }}>Shipped with {delivService}</p>
+                                    <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#D3D3D3' }}>Shipped with {delivService}</p>
                                 </div>
                             </div>
                             <div className="p-3 shadow-md" style={{ backgroundColor: '#ffffff' }}>
@@ -107,7 +107,7 @@ export default function ShipDetails() {
                                             <button className="text-red-500 text-m lg:text-sm font-semibold self-start mr-2 font-vietnam" style={{ color: '#5D9EA4' }}>Track</button>
                                         </div>
                                         <div className="flex items-center">
-                                            <img src={darkgreenpoint} alt="Red Point" className="w-3 h-7 mr-2" />
+                                            <img src={darkgreenpoint} alt="Dark Green Point" className="w-3 h-7 mr-2" />
                                             <div className="flex flex-col">
                                                 <span className="text-xs lg:text-sm font-bold font-vietnam" style={{ color: '#4D946D' }}>Package received by delivery service</span>
                                                 <span className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>{preparingTime} </span>
@@ -118,16 +118,15 @@ export default function ShipDetails() {
                             </div>
                         </>
                     )}
-
                     {shipmentState === 'Missing' && (
                         <>
                             <div className="p-3 shadow-md flex items-start" style={{ backgroundColor: '#CD484870' }}>
-                                <img src={RedTruck} alt="Package" className="w-6 h-5 mr-2 " style={{ color: '#000000' }} />
+                                <img src={RedTruck} alt="Package" className="w-6 h-5 mr-2" />
                                 <div className="flex flex-col justify-between">
                                     <h2 className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#852222' }}>Missing</h2>
                                     <p className="text-xs lg:text-sm font-vietnam">
-    This package is missing. <strong style={{fontWeight: 'bold'}}>Contact</strong> <strong style={{fontWeight: 'bold', textDecoration: 'underline'}}>Harbour Guard</strong> for confirmation or <strong style={{fontWeight: 'bold'}}>Delivery Service</strong> for <strong style={{fontWeight: 'bold'}}>further information.</strong>
-</p>
+                                        This package is missing. <strong style={{fontWeight: 'bold'}}>Contact</strong> <strong style={{fontWeight: 'bold', textDecoration: 'underline'}}>Harbour Guard</strong> for confirmation or <strong style={{fontWeight: 'bold'}}>Delivery Service</strong> for <strong style={{fontWeight: 'bold'}}>further information.</strong>
+                                    </p>
                                     <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>Shipped with {delivService}</p>
                                 </div>
                             </div>
@@ -155,33 +154,62 @@ export default function ShipDetails() {
                             </div>
                         </>
                     )}
-
+                    {shipmentState === 'Completed' && (
+                        <>
+                            <div className="p-3 shadow-md flex items-start" style={{ backgroundColor: '#848948' }}>
+                                <img src={neoTruck} alt="Package" className="w-6 h-5 mr-2" />
+                                <div className="flex flex-col justify-between">
+                                    <h2 className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#F2F9A9' }}>Completed</h2>
+                                    <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#FFFFFF' }}>Estimated Arrival <strong>{date}</strong></p>
+                                    <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#D3D3D3' }}>Shipped with {delivService}</p>
+                                </div>
+                            </div>
+                            <div className="p-3 shadow-md" style={{ backgroundColor: '#ffffff' }}>
+                                <div className="flex">
+                                    <img src={point} alt="Neo Point" className="h-6 mr-3" />
+                                    <div className="flex-grow">
+                                        <div className="flex justify-between items-center">
+                                            <div>
+                                                <h2 className="font-bold text-md lg:text-lg font-vietnam">Shipping Information</h2>
+                                                <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>{delivType}</p>
+                                                <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>{delivStatus}</p>
+                                            </div>
+                                            <button className="text-red-500 text-m lg:text-sm font-semibold self-start mr-2 font-vietnam" style={{ color: '#5D9EA4' }}>Track</button>
+                                        </div>
+                                        <div className="flex items-center">
+                                            <img src={neoPoint} alt="Neo Point" className="w-3 h-7 mr-2" />
+                                            <div className="flex flex-col">
+                                                <span className="text-xs lg:text-sm font-bold font-vietnam" style={{ color: '#848948' }}>Package received by Orang Rumah</span>
+                                                <span className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282' }}>{preparingTime} </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
                     <div className="batch-info-container p-3 shadow-md bg-white" style={{ paddingBottom: '30px' }}> 
                         <div>
                             <h2 className="font-bold text-md lg:text-lg font-vietnam">Batch Information</h2>
                             <hr className="mt-1" style={{ borderColor: '#E5E7EB', borderWidth: '1px', height: 0 }} />
                         </div>
                         {batches.map((batch, index) => (
-                    <React.Fragment key={batch.id}>
-                        <div className="flex justify-between items-start mt-2 text-">
-                        <div className="text-left">
-
-                            <p className="font-bold text-md lg:text-lg font-vietnam">Batch #{batch.id}</p>
-                            <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}>Dried Date</p>
-                            <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}>Floured Date</p>
-                            </div>
-                            <div className="text-right">
-                                <span className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#5D9EA4' }}>{batch.weight}</span>
-                                <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}> {batch.driedDate}</p>
-                                <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}> {batch.flouredDate}</p>
-                            </div>
-                        </div>
-                        {index < batches.length - 1 && <hr className="my-2" />}
-                    </React.Fragment>
-                ))}
-
-
-
+                            <React.Fragment key={batch.id}>
+                                <div className="flex justify-between items-start mt-2">
+                                    <div className="text-left">
+                                        <p className="font-bold text-md lg:text-lg font-vietnam">Batch #{batch.id}</p>
+                                        <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}>Dried Date</p>
+                                        <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}>Floured Date</p>
+                                    </div>
+                                    <div className="text-right">
+                                        <span className="font-bold text-md lg:text-lg font-vietnam" style={{ color: '#5D9EA4' }}>{batch.weight}</span>
+                                        <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}> {batch.driedDate}</p>
+                                        <p className="text-xs lg:text-sm font-vietnam" style={{ color: '#828282'}}> {batch.flouredDate}</p>
+                                    </div>
+                                </div>
+                                {index < batches.length - 1 && <hr className="my-2" />}
+                            </React.Fragment>
+                        ))}
                         <hr className="my-2" /> 
                         <div className="flex justify-between">
                             <h3 className="font-bold text-md lg:text-lg font-vietnam">Total Weight</h3>
@@ -199,16 +227,15 @@ export default function ShipDetails() {
                         </button>
                     </div>
                     <footer className="bg-gray-200 text-black flex justify-between items-center h-10 px-3 fixed bottom-0 left-0 right-0">
-                    <p className="font-semibold">@2024 AMIN</p>
-                    <p className="font-semibold">CENTRA</p>
-                </footer>
+                        <p className="font-semibold">@2024 AMIN</p>
+                        <p className="font-semibold">CENTRA</p>
+                    </footer>
                 </>
             ) : (
                 <div className="flex justify-center items-center h-screen">
                     <p className="text-gray-600">Not available for this device.</p>
                 </div>
             )}
-            </div>
-        
+        </div>
     );
 }
