5a696ec03c3f64ad0bb24a3179cb0f8205abe82f
audreyfabiola <clarissaaudreyy@gmail.com>
2024-05-17 22:12:29 +0700

    Edit XYZHome and Addeed Assets components

--- Mori/src/components/XYZ/Mobile/XYZHome.jsx
+++ Mori/src/components/XYZ/Mobile/XYZHome.jsx
@@ -7,10 +7,11 @@ import { useState } from 'react';
 import moriLogo from '../../../assets/moriWhite.png';
 import bell from '../../../assets/bell.png';
 import hamburg from '../../../assets/hamburg.png';
-import bg from '../../../assets/usercardBG.png';
+import bg from '../../../assets/usercardBG2.png';
 import collector from '../../../assets/collectorLogo.png';
 import processor from '../../../assets/processorLogo.png';
 import shipping from '../../../assets/shippingLogo.png';
+import truck from '../../../assets/shippingTruck.png';
 
 const gaugeOptions = {
   responsive: true,
@@ -30,44 +31,49 @@ export default function XYZHome() {
   const isMobile = width <= 640;
 
   const [machinesData, setMachines] = useState([
-    { number: 1, status: 'FULL', currentLoad: 24, capacity: 30, lastUpdated: '1 hour ago' },
-    { number: 2, status: 'FULL', currentLoad: 30, capacity: 30, lastUpdated: '2 hours ago' },
-    { number: 3, status: 'EMPTY', currentLoad: 10, capacity: 30, lastUpdated: '30 minutes ago' },
+    { location: 'Kecamatan Semau', status: 'FULL', currentLoad: 31.1, capacity: 50, lastUpdated: '1 hour ago' },
   ]);
   
   const renderMachines = () => {
     return machinesData.map((machine, index) => (
-      <MachineCard key={machine.number} machine={machine} extraMarginClass={index === machinesData.length - 1 ? 'mb-10' : 'mb-4'} />
+      <MachineCard key={machine.location} machine={machine} extraMarginClass={index === machinesData.length - 1 ? 'mb-10' : 'mb-4'} />
     ));
   };
   
   const MachineCard = ({ machine, extraMarginClass }) => {
-    const chartColor = machine.currentLoad === machine.capacity ? '#0F3F43' : (machine.currentLoad > machine.capacity / 2 ? '#5D9EA4' : '#99D0D580');
-  
-    const linkTo = `/dryingmachine/${machine.number}`;
+    const chartColor = machine.currentLoad === machine.capacity ? '#0F3F43' : (machine.currentLoad > machine.capacity / 2 ? '#A7AD6F' : '#99D0D580');
+    const linkTo = `/dryingmachine/${machine.location}`;
+    
+    const [region, location] = machine.location.split(' ');
   
     return (
+
       <div className={`machine-card bg-white p-4 rounded-lg shadow ${extraMarginClass} flex flex-col items-center font-vietnam ${machine.status.toLowerCase()}`} style={{ width: 'auto', flexGrow: 1, minWidth: '300px', minHeight: '100px', maxWidth: 'none', position: 'relative' }}>
-        <div className="machine-number bg-black text-white rounded-full h-6 w-6 flex items-center justify-center" style={{ position: 'absolute', left: '15px', top: '15px' }}>
-          <span className="font-bold text-sm">{machine.number}</span>
-        </div>
-        <div className="machine-info flex justify-center items-center w-full mb-2">
-          <div className="chart-container" style={{ width: '150px', height: '120px', position: 'relative' }}>
-            <Doughnut data={{ labels: ['Current Load', 'Capacity'], datasets: [{ data: [machine.currentLoad, machine.capacity - machine.currentLoad], backgroundColor: [chartColor, '#EFEFEF'], borderWidth: 0 }] }} options={gaugeOptions} />
-            <div className="absolute inset-0 flex flex-col items-center justify-center" style={{ fontSize: '0' }}>
-              <span className="font-vietnam font-bold" style={{ fontSize: '24px', lineHeight: '1.2' }}>{machine.currentLoad} kg</span>
-              <span className="font-vietnam font-bold" style={{ fontSize: '12px', lineHeight: '1.2', marginBottom: '-30px' }}>{`/ ${machine.capacity} kg`}</span>
-            </div>
+      {/* Machine Location Button */}
+      <div className="machine-location w-[170px] h-[26px] px-2.5 py-1 bg-black rounded justify-start items-center gap-2 inline-flex text-white text-sm font-medium font-['Be Vietnam Pro']" style={{ position: 'absolute', left: '15px', top: '9px' }}>
+        <span className="text-sm">{region} </span><span className="font-bold text-sm">{location}</span>
+        <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 5C6 5.13464 5.9724 5.26019 5.9172 5.37664C5.86569 5.48945 5.78289 5.60044 5.66881 5.70961L1.50138 9.74891C1.33211 9.9163 1.12603 10 0.883165 10C0.724931 10 0.577737 9.95997 0.441582 9.87991C0.305428 9.80349 0.196872 9.69978 0.115915 9.56878C0.0386385 9.43777 0 9.29221 0 9.1321C0 8.89192 0.0938362 8.67722 0.281509 8.48799L3.91904 4.99454L0.281509 1.50655C0.0938362 1.3246 0 1.11172 0 0.867904C0 0.707787 0.0386385 0.562227 0.115915 0.431223C0.196872 0.300218 0.305428 0.196507 0.441582 0.120087C0.577737 0.0400291 0.724931 0 0.883165 0C1.12603 0 1.33211 0.0818777 1.50138 0.245633L5.66881 4.28493C5.78289 4.3941 5.86569 4.50691 5.9172 4.62336C5.96872 4.73617 5.99632 4.86172 6 5Z" fill="white"/>
+        </svg>
+      </div>
+        
+        {/* Machine Info */}
+        <div className="machine-info flex justify-center items-center w-full mb-2" style={{ marginTop: '18px' }}>
+        <div className="chart-container" style={{ width: '150px', height: '120px', position: 'relative' }}>
+          <Doughnut data={{ labels: ['Current Load', 'Capacity'], datasets: [{ data: [machine.currentLoad, machine.capacity - machine.currentLoad], backgroundColor: [chartColor, '#EFEFEF'], borderWidth: 0 }] }} options={gaugeOptions} />
+          <div className="absolute inset-0 flex flex-col items-center justify-center" style={{ fontSize: '0' }}>
+            <span className="font-vietnam font-bold" style={{ fontSize: '24px', lineHeight: '1.2' }}>{machine.currentLoad} kg</span>
+            <span className="font-vietnam font-bold" style={{ fontSize: '12px', lineHeight: '1.2', marginBottom: '-30px' }}>{`/ ${machine.capacity} kg`}</span>
           </div>
         </div>
-        <div className="last-updated" style={{ position: 'absolute', top: '5px', right: '5px', fontSize: '10px', color: '#666666' }}>
-          <div>Last updated:</div>
-          <div style={{ fontWeight: 'bold' }}>{machine.lastUpdated}</div>
-        </div>
       </div>
+      <div className="last-updated" style={{ position: 'absolute', top: '5px', right: '5px', fontSize: '10px', color: '#666666' }}>
+        <div>Last updated:</div>
+        <div style={{ fontWeight: 'bold' }}>{machine.lastUpdated}</div>
+      </div>
+    </div>
     );
   };
-  
 
   return (
     <div>
@@ -102,39 +108,55 @@ export default function XYZHome() {
             </div>
           </header>
 
+          {/* Current Stock Management */}
           <div className="p-5">
             <div className="mt-">
-              <h2 className="text-gray-600 font-bold">Quick Access</h2>
-            </div>
-            <div className="flex">
-              <Link to="/collector">
-                <div className="mt-3 mr-3 rounded-lg flex justify-center items-center relative" style={{ width: "110px", height: "100px", backgroundColor: "#5D9EA4"}}>
-                  <img src={collector} alt="mori logo" className="absolute top-3 right-2 w-6" style={{ marginTop: "5px", marginRight: "5px" }} />
-                  <p className="text-white text-center font-semibold absolute bottom-3 left-2" style={{ marginBottom: "5px", marginLeft: "5px" }}>Collector</p>
-                </div>  
-              </Link>
-              <Link to="/processor">
-                <div className="mt-3 mr-3 rounded-lg flex justify-center items-center relative" style={{ width: "110px", height: "100px", backgroundColor: "#4D946D"}}>
-                  <img src={processor} alt="mori logo" className="absolute top-3 right-2 w-6" style={{ marginTop: "5px", marginRight: "5px" }} />
-                  <p className="text-white text-center font-semibold absolute bottom-3 left-2" style={{ marginBottom: "5px", marginLeft: "5px" }}>Processor</p>
-                </div> 
-              </Link>
-              <Link to="/shipping">
-                <div className="mt-3 mr-3 rounded-lg flex justify-center items-center relative" style={{ width: "110px", height: "100px", backgroundColor: "#606060"}}>
-                  <img src={shipping} alt="mori logo" className="absolute top-3 right-2 w-6" style={{ marginTop: "5px", marginRight: "5px" }} />
-                  <p className="text-white text-center font-semibold absolute bottom-3 left-2" style={{ marginBottom: "5px", marginLeft: "5px" }}>Shipping</p>
-                </div> 
-              </Link>
-            </div>
-          </div>
-
-            {/* Machine Stock Status */}
-          <div className="p-5">
-          <div className="mt-">
-              <h2 className="text-gray-600 font-bold ">Machine Stock Status</h2>
+              <h2 className="text-black text-xl font-bold font-['Be Vietnam Pro']">Current Stock Management</h2>
             </div>
             <div className="machine-status my-4">{renderMachines()}</div>
           </div>
+
+          {/* Quick Access */}
+          <div className="p-5">
+            <div className="mt-[-40px]">
+              <h2 className="text-black text-xl font-bold font-['Be Vietnam Pro']">Quick Access</h2>
+            </div>
+            <div className="flex">
+            {/* Stock */}
+            
+            <Link to="/collector">
+            <div className="mt-3 mr-3 w-[166px] h-[74px] px-4 py-3 bg-stone-600 rounded-lg flex-col justify-start items-end inline-flex" style={{ position: 'relative', backgroundColor: '#5C612C' }}>
+                <img src={shipping} alt="mori logo" className="absolute top-3 right-2 w-6" style={{ marginTop: '5px', marginRight: '5px' }} />
+                <div className="w-[18.65px] h-5 relative" />
+                <div className="self-stretch h-[50px] flex-col justify-start items-start gap-0.25 flex">
+                <div className="self-stretch text-white text-[15px] font-bold font-['Be Vietnam Pro']">Stock Management</div>
+                <div className="self-stretch text-white text-[8px] font-normal font-['Be Vietnam Pro']">2 Warehouse almost FULL</div>
+                </div>
+            </div>
+            </Link>
+
+            {/* Shipping */}
+              <Link to="/shipping">
+              <div className="mt-3 mr-3 w-[166px] h-[74px] px-4 py-3 bg-stone-600 rounded-lg flex-col justify-start items-end inline-flex" style={{ position: 'relative', backgroundColor: '#00000099' }}>
+                <img src={truck} alt="mori logo" className="absolute top-3 right-2 w-6" style={{ marginTop: '5px', marginRight: '5px' }} />
+                <div className="w-[18.65px] h-5 relative" />
+                <div className="self-stretch h-[50px] flex-col justify-start items-start gap-0.25 flex">
+                <div className="self-stretch text-white text-[15px] font-bold font-['Be Vietnam Pro']">Shipping Information</div>
+                <div className="self-stretch text-white text-[8px] font-normal font-['Be Vietnam Pro']">4 Shipment Arriving</div>
+                </div>
+            </div>
+              </Link>
+            </div>
+
+            {/* Recent Shipping Status */}
+            <div className="pt-5">
+            <div className="mt-[20px]">
+              <h2 className="text-black text-xl font-bold font-['Be Vietnam Pro']">Recent Shipping Status</h2>
+            </div>
+            </div>
+
+          </div>
+          
 
           <footer className="bg-gray-200 text-black flex justify-between items-center h-10 px-3 fixed bottom-0 left-0 right-0">
             <p className="font-semibold">@2024 AMIN</p>
@@ -149,4 +171,3 @@ export default function XYZHome() {
     </div>
   );
 }
-
