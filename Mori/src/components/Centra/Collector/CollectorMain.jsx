0bbeffb0a5ea5b36721c1edacde03df2aee91f4a
audreyfabiola <clarissaaudreyy@gmail.com>
2024-06-07 22:45:28 +0700

    Fixed Collector Main

--- Mori/src/components/Centra/Collector/CollectorMain.jsx
+++ Mori/src/components/Centra/Collector/CollectorMain.jsx
@@ -4,16 +4,11 @@ import DropDown from "./DropDown";
 import EditBatch from "./EditBatch"; // Import the EditBatch component
 import React, { useState, useEffect } from "react";
 
-const CollectorMain = ({ totalWeight }) => {
+const CollectorMain = () => {
   const { width } = useWindowSize(); // Get the window width using the useWindowSize hook
 
   const isMobile = width <= 640;
   const footerHeight = 40;
-
-  // Define date, weight, and time
-  const [date, setDate] = useState("");
-  const [weight, setWeight] = useState("");
-  const [time, setTime] = useState("");
 
   const [batchData, setBatchData] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
@@ -25,57 +20,19 @@ const CollectorMain = ({ totalWeight }) => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  // Calculate the total weight
+  const totalWeight = batchData.reduce((total, batch) => total + (parseFloat(batch.weight) || 0), 0);
+
   const handleClose = () => {};
 
   return (
     <div className="collector-main-container h-screen bg-[#F0F0F0] overflow-hidden flex flex-col items-start justify-start pt-[18px] px-0 pb-0 box-border gap-[24px] leading-[normal] tracking-[normal] ml-auto mr-auto overflow-y-auto">
       {/* Header */}
       <header className="self-stretch flex flex-col items-start justify-start gap-[24px] max-w-full text-left text-base text-black font-vietnam">
-        <nav className="m-0 self-stretch flex flex-row items-start justify-start py-0 pr-6 pl-5 box-border max-w-full">
-          <nav className="m-0 flex-1 flex flex-row items-start justify-between max-w-full gap-[20px] text-right text-xl text-gray-100 font-vietnam">
-            <div className="flex flex-row items-start justify-start gap-[20px]">
-              <div className="flex flex-row items-start justify-start">
-                <h3 className="m-0 w-6 relative text-inherit tracking-[-0.02em] font-semibold font-inherit inline-block min-w-[24px]">
-                  <svg
-                    className="w-[26px] h-[26px] text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="26"
-                    height="26"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2.5"
-                      d="M5 12h14M5 12l4-4m-4 4 4 4"
-                    />
-                  </svg>
-                </h3>
-              </div>
-              <h3 className="m-0 relative text-inherit font-bold font-vietnam text-gray-950 text-left inline-block min-w-[89px]">
-                Collectors
-              </h3>
-            </div>
-            <div className="flex flex-row items-start justify-start gap-[15px] text-left text-black">
-              <div className="flex flex-col items-start justify-start pt-px px-0 pb-0">
-                <h3 className="m-0 relative text-inherit tracking-[-0.02em] font-bold font-inherit inline-block min-w-[24px]">
-                  <svg
-                    className="w-[26px] h-[26px] text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z" />
-                  </svg>
-                </h3>
-              </div>
-              <h3 className="m-0 relative text-[22px] tracking-[-0.02em] font-bold font-inherit inline-block min-w-[28px]">
+        <nav className="m-0 self-stretch flex flex-row items-center justify-between py-0 pr-6 pl-5 box-border max-w-full">
+          <div className="flex flex-row items-center justify-start gap-[20px]">
+            <h3 className="m-0 relative text-[22px] tracking-[-0.02em] font-bold font-inherit inline-block min-w-[28px]">
+              <a href="centranavigation">
                 <svg
                   className="w-[30px] h-[30px] text-gray-800"
                   aria-hidden="true"
@@ -92,9 +49,33 @@ const CollectorMain = ({ totalWeight }) => {
                     d="M5 7h14M5 12h14M5 17h14"
                   />
                 </svg>
-              </h3>
-            </div>
-          </nav>
+              </a>
+            </h3>
+          </div>
+
+          <div className="flex flex-1 flex-row items-center justify-center">
+            <h3 className="m-0 relative text-inherit font-bold font-vietnam text-gray-950 text-center inline-block min-w-[89px]">
+              Collectors
+            </h3>
+          </div>
+
+          <div className="flex flex-row items-center justify-end gap-[15px] text-left text-black">
+            <h3 className="m-0 relative text-inherit tracking-[-0.02em] font-bold font-inherit inline-block min-w-[24px]">
+              <a href="centranotif">
+                <svg
+                  className="w-[26px] h-[26px] text-gray-800"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z" />
+                </svg>
+              </a>
+            </h3>
+          </div>
         </nav>
       </header>
 
@@ -113,7 +94,7 @@ const CollectorMain = ({ totalWeight }) => {
             </div>
             <div className="flex flex-col items-end">
               <span className="text-black text-4xl font-bold font-['Be Vietnam Pro']">
-                54.6{" "}
+                {totalWeight.toFixed(1)}{" "}
                 <span className="text-black text-xl font-medium font-['Be Vietnam Pro']">
                   kg
                 </span>{" "}
@@ -129,7 +110,7 @@ const CollectorMain = ({ totalWeight }) => {
             <DropDown className="w-full" />
           </div>
 
-          {/* Render BatchBox components using fetched data
+          {/* Render BatchBox components using fetched data */}
           {batchData.map(batch => (
             <BatchBox
               key={batch.batchId}
@@ -141,7 +122,7 @@ const CollectorMain = ({ totalWeight }) => {
               duration={batch.duration}
               selectedDate={selectedDate}
             />
-          ))} */}
+          ))}
 
           {/* <EditBatch onClose={handleClose} batchData={{}} date={date} weight={weight} time={time} /> */}
         </div>
