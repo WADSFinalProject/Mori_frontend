09c79222ef34b760fc8b1d025b44d0d412294a6a
Tirza Gabriella <tirzagabriella20@gmail.com>
2024-06-16 12:05:11 +0700

    reverted be call back to live url

--- Mori/src/service/config.js
+++ Mori/src/service/config.js
@@ -1,2 +1,2 @@
-// export const host = "https://mori-backend.vercel.app"
-export const host = "http://localhost:8000"
+export const host = "https://mori-backend.vercel.app"
+// export const host = "http://localhost:8000"
