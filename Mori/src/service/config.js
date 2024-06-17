5c1db8fff76fcb6ad343aab78f997bfb9dd54cb7
audreyfabiola <clarissaaudreyy@gmail.com>
2024-06-18 01:53:09 +0700

    XYZ-Accepted-Packages

--- Mori/src/service/config.js
+++ Mori/src/service/config.js
@@ -1,3 +1,3 @@
-export const host = "https://mori-backend.vercel.app"
-// export const host = "http://127.0.0.1:8000"
+// export const host = "https://mori-backend.vercel.app"
+export const host = "http://127.0.0.1:8002"
 
