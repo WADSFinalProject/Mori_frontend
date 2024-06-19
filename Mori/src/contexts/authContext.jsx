55cfd1eb1b7bdfa662747e8d98aa1f507cfa0c2c
Francesco Emmanuel <114067626+FrancescoEmmanuel@users.noreply.github.com>
2024-06-20 00:46:14 +0700

    a

--- Mori/src/contexts/authContext.jsx
+++ Mori/src/contexts/authContext.jsx
@@ -1,6 +1,7 @@
+// AuthContext.js
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {jwtDecode} from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
@@ -15,27 +16,26 @@ export const AuthProvider = ({ children }) => {
     const saveAccessToken = (token) => {
         setAccessToken(token);
         const decoded = jwtDecode(token);
-        setUserRole(decoded.role); // Ensure your token actually includes a 'role' claim
+        setUserRole(decoded.role);
     };
 
     const refreshAccessToken = async () => {
         try {
-            const response = await axios.post("http://localhost:8000/token/refresh", {}, {
+            const response = await axios.post(`${API_URL}/token/refresh`, {}, {
                 withCredentials: true,
             });
             const newAccessToken = response.data.access_token;
             saveAccessToken(newAccessToken);
-            return newAccessToken; // Return new token for interceptor to use
+            return newAccessToken;
         } catch (error) {
             console.log("Error refreshing token: ", error);
             toast.error("Session expired. Please log in again.", { position: "top-center" });
             navigate('/');
-            throw error; // Rethrow to handle by interceptor
+            throw error;
         }
     };
 
     useEffect(() => {
-        // Call setupInterceptors from here once we define it in api.js
         import('./api').then(apiModule => {
             apiModule.setupInterceptors(accessToken, saveAccessToken, refreshAccessToken);
         });
@@ -49,3 +49,4 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () => useContext(AuthContext);
+
